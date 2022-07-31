758c10bff7d4289fa65ca7bd8ba866862b17c534
jissjoseph1997 <jissjoseph14@gmail.com>
2022-07-31 10:17:40 +0000

    h

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -5,11 +5,9 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def test_mux(dut):
-
-     """Test for mux2"""
+    """Test for mux2"""
     cocotb.log.info('##### CTB: Develop your test here ########')
-    var = [inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17, inp18, inp19, inp20,inp21, inp22, inp23, inp24, inp25, inp26, inp27, inp28, inp29] 
-    var = [i for i in range(30)]
+    var = [inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17, inp18, inp19, inp20,inp21, inp22, inp23, inp24, inp25, inp26, inp27, inp28, inp29] = [i for i in range(30)]
 
     num = 0
     for i in var:
