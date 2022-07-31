43bd221cbcb7483c00638cbbae11d379efa0421e
jissjoseph1997 <jissjoseph14@gmail.com>
2022-07-31 10:00:42 +0000

    n

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,7 +8,7 @@ async def test_mux(dut):
     """Test for mux2"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
- var = [inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17, inp18, inp19, inp20,inp21, inp22, inp23, inp24, inp25, inp26, inp27, inp28, inp29]
+                 var = [inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17, inp18, inp19, inp20,inp21, inp22, inp23, inp24, inp25, inp26, inp27, inp28, inp29]
          num = 0
          for i in var:
              dut.i = i = num
