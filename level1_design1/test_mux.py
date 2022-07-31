f78e6897398635b88ce31c196045a71d7d1bf922
jissjoseph1997 <jissjoseph14@gmail.com>
2022-07-31 10:02:16 +0000

    h

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -9,15 +9,15 @@ async def test_mux(dut):
 
     cocotb.log.info('##### CTB: Develop your test here ########')
     var = [inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17, inp18, inp19, inp20,inp21, inp22, inp23, inp24, inp25, inp26, inp27, inp28, inp29]
-         num = 0
-         for i in var:
-             dut.i = i = num
-             num+=1
-             count = 0
-         while count < 29:
-                 dut.sel = bin(count)
-                 assert dut.out == var[i], f"MUX result is incorrect: {dut.out.value} != {count}"
-                 count+=1
+    num = 0
+    for i in var:
+        dut.i = i = num
+        num+=1
+     count = 0
+    while count < 29:
+        dut.sel = bin(count)
+        assert dut.out == var[i], f"MUX result is incorrect: {dut.out.value} != {count}"
+        count+=1
 
     
     
