f21651a3f7ed22b6e12ff4fa3724574798813008
jissjoseph1997 <jissjoseph14@gmail.com>
2022-07-31 10:12:43 +0000

    g

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -5,6 +5,9 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def test_mux(dut):
+    cocotb.log.info("About to wait for 10 ns")
+    yield Timer(10, units='ns')
+    cocotb.log.info("Simulation time has advanced by 10 ns")
     """Test for mux2"""
     cocotb.log.info('##### CTB: Develop your test here ########')
     var = [inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17, inp18, inp19, inp20,inp21, inp22, inp23, inp24, inp25, inp26, inp27, inp28, inp29]
